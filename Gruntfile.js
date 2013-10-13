10b5afa0e0117bee48f730d2997295d4fa29e2b8
= <=>
2013-10-13 21:12:36 +0200

    do not rev:dist sounds

--- Gruntfile.js
+++ Gruntfile.js
@@ -166,8 +166,7 @@ module.exports = function (grunt) {
             '<%= yeoman.dist %>/scripts/vendor/{,*/}*.js',
             '<%= yeoman.dist %>/styles/{,*/}*.css',
             '<%= yeoman.dist %>/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}',
-            '<%= yeoman.dist %>/styles/fonts/*',
-            '<%= yeoman.dist %>/sounds/*'
+            '<%= yeoman.dist %>/styles/fonts/*'
           ]
         }
       }
